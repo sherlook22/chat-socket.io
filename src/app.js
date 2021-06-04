791e2ed8038bcf51267719cb01f5191255dce5cf
Gonzalo Re <gonzalore01@gmail.com>
2021-06-04 20:53:21 -0300

    feat: add socketio config to start socket

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 const app = require('express')()
+const socketio = require('socket.io')
+const http = require('http')
 const router = require('./router/index')
 
 app.use('/api', router)
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', (socket) => {
+  console.log('New connection started')
+
+  socket.on('disconnect', () => {
+    console.log('Disconect')
+  })
+})
 
 module.exports = app
