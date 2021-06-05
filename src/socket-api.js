11ffaccd2eaf6ee3a407f50974c132b9df1a6f12
Gonzalo Re <gonzalore01@gmail.com>
2021-06-05 17:31:12 -0300

    feat: create the socket to add user for rooms

--- src/socket-api.js
+++ src/socket-api.js
@@ -1,5 +1,24 @@
+const { addUser } = require('./users')
+
 module.exports = (io) => {
-  io.on('connect', (socket) => {
-    console.log('New connection')
+  io.on('connection', (socket) => {
+    console.log('New connection started')
+
+    socket.on('join', ({ name, room }, callback) => {
+      const { error, user } = addUser()
+
+      if (error) return callback(error)
+
+      socket.emit('message', { user: 'admin', text: `${user.name}, welcom to the room ${user.room}` })
+      socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.room}, has joined` })
+
+      socket.join(user.room)
+
+      callback()
+    })
+
+    socket.on('disconnect', () => {
+      console.log('User had left')
+    })
   })
 }
